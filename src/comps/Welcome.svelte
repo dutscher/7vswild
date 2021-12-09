7edb563b76ec3b49e8ca739967ddbca360b3bf0a
Willy Woitas <willy.woitas@neofonie.de>
2021-12-09 22:42:41 +0100

    test github actions

--- src/comps/Welcome.svelte
+++ src/comps/Welcome.svelte
@@ -1,23 +1,12 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import { localStore } from "../stores/local-storage";
-    // app
 
-    let activeTagIds;
-    let activePartIds;
-    let activeColorIds;
-    let activeStateIds;
-    let activeSearchString;
-    let loadedChanges;
     let tags: number = 0;
     let products: number = 0;
     let isTouched = false;
 
-    $: isFiltered = !isTouched && (!!activeSearchString
-        || activeTagIds.length > 0
-        || activeStateIds.length > 0
-        || activeColorIds.length > 0
-        || activePartIds.length > 0);
+    $: isFiltered = !isTouched;
 
     $: isVisible = !isFiltered;
 
@@ -37,22 +26,14 @@
 </script>
 
 <h1 class="with-toggle" on:click={onClick}>
-    Welcom
+    7 Vs Wild
 </h1>
 <div class="welcome{isVisible ? ' show' : ''}">
     <div class="welcome__content">
         <p>
-            Dies ist ein von der Community gebauter Kompakter Bluebrixx only Produkte Katalog.<br/>
-            Aufgrund des vielen klickens und langer Ladezeiten im BB eigenen Online-Shop haben wir das hier entwickelt.
-            <b>Wir sind nicht Bluebrixx</b>.
-        <p>
-            Wähle einen der <b>{tags} Tags</b> (Jedes hat einen Deeplink in der URL) aus, suche nach einem von
-            <b>{products} Bluebrixx Produkten</b> oder
-            wähle einen Verfügbarkeit Status, außerdem gibt es noch einen Status-Verlauf in der Detailansicht.<br/>
-            Zu jedem Produkt gibts noch die passende Anleitung (Zum herrunterladen müsst ihr aber bei BB eingeloggt
-            sein)<br/>
-            Aber Obacht: es gibt noch 3 Überraschungen ;)<br/>
-            Viel Spaß
+            Dies ist ein von der Community gebaute Übersichtsseite.<br/>
+            Hier seht ihr den zwischen Stand, die Challenge Ergebnisse und zu allen Videos die Reactions der Kandidaten.<br />
+            <b>Viel Spaß damit und Grüße gehen raus an Fritz für das geile Projekt</b>.
         </p>
     </div>
 </div>
