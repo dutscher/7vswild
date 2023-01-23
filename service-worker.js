fa17c1334f85e67f8f895e5a362c64e8c1744473
dutscher <dutscher_sbf@hotmail.com>
2023-01-23 20:58:13 +0000

    Deploying to gh-pages from @ dutscher/7vswild@f16cd437c58d9b1338692bcbeefd253694d02131 🚀

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+// Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+const pre = '[ServiceWorker]';
+// notification and product store
+const store = {};
+
+// Add list of files to cache here.
+const FILES_TO_CACHE = [
+    './index.html',
+    './offline.html',
+];
+
+self.addEventListener('install', (evt) => {
+    console.log(pre, 'install');
+
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log(pre, 'activate');
+    // Remove previous cached data from disk.
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    //console.log(pre, 'fetch', evt.request.url);
+    // Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('offline.html');
+                    });
+            })
+    );
+});
